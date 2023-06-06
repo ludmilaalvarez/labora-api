c7f9521b4bafafc9f1998c16555701249d812439
ludmilaalvarez <ludmi096@gmail.com>
2023-06-06 17:46:12 -0300

    billetera

--- Api-Wallet/models/interfaces.go
+++ Api-Wallet/models/interfaces.go
@@ -3,6 +3,7 @@ package models
 type DBHandlerWallet interface {
 	CrearWallet(Datos *Datos_Solicitados) (int, error)
 	StatusWallet(dni string) (Wallets, error)
+	DeleteWallet(person_id string) error
 }
 
 type DBHandlerLog interface {
